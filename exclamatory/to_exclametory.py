9ddaf1d21106542d1a13e0193188f1300ea0c095
Ajin Jacob <jacoaji02@gmail.com>
2026-06-21 12:40:49 +0530

    Add files via upload

---
+++ exclamatory/to_exclametory.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+# Read CSV
+df = pd.read_csv("dataset.csv")
+
+# Select rows where transcript ends with !
+sentences_with_exclamation = df[
+    df["transcript"].astype(str).str.contains(r'!\s*$', regex=True, na=False)
+]
+
+# Count
+count = len(sentences_with_exclamation)
+
+print(sentences_with_exclamation)
+print("Number of sentences ending with '!':", count)
+
+# Save to a new CSV file
+sentences_with_exclamation.to_csv(
+    "sentences_ending_with_exclamation.csv",
+    index=False
+)
+
+print("Saved to sentences_ending_with_exclamation.csv")
+
